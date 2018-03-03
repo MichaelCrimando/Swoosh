e9ac5e11c0dbded73fe6c7265f6264dd4beb9abf
Michael Crimando <crimandomichael@yahoo.com>
2018-03-02 23:34:44 -0500

    First lesson complete

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,20 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Angela Coleman on 3/2/18.
+//  Copyright © 2018 Big Dev Industries. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //This isnt in the builder, so we gotta code it
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
