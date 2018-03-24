748df4f29d8e1bf1f5087ad3f529e9478b152a65
Michael Crimando <crimandomichael@yahoo.com>
2018-03-24 11:09:07 -0400

    Lesson 09 complete for real this time

---
+++ Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Michael Crimando on 3/24/18.
+//  Copyright © 2018 Big Dev Industries. All rights reserved.
+//
+
+import Foundation
--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    
+    var player: Player!
+    @IBOutlet weak var NextBtn: BorderButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    @IBAction func OnMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func OnWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    @IBAction func OnCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
     
+    func selectLeague(leagueType: String) {
+        player.desiredLeauge = leagueType
+        NextBtn.isEnabled = true
+    }
 }
